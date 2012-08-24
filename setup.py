d1f73df61fd9476d4f022adbbd11a3ba2661a4bf
Gene Kong <gyx_edu@qq.com>
2012-08-24 19:37:08 +0800

    update for setup.py! now we can build it with python in both windows and linux.

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import glob
+import os
+import os.path
+import sys
+
+#All c source
+c_src = glob.glob(os.path.join("src","*.c"))
+
+inc_dirs = []
+lib_dirs = []
+libs     = []
+
+MTI_HOME = ""
+VCS_HOME = ""
+
+#Try get modelsim/vcs home
+if os.environ.has_key("MTI_HOME") :
+    MTI_HOME = os.environ["MTI_HOME"]
+elif os.environ.has_key("VCS_HOME") :
+    VCS_HOME = os.environ['VCS_HOME']
+else :
+    for path in os.environ["PATH"].split(";") :
+        if not os.path.lexists(path) :
+            continue
+        all_files = os.listdir(path)
+        if "vsim.exe" in all_files or \
+        "vsim" in all_files:
+            MTI_HOME = os.path.join(path,"..")                                        
+            break
+        elif "vcs.exe" in all_files or \
+        "vcs" in all_files:
+            VCS_HOME = os.path.join(path,"..")
+            break
+
+#update include/libs dirs.
+if MTI_HOME :
+    inc_dirs.append(os.path.join(MTI_HOME,"include"))
+    if os.environ["OS"] == "Windows_NT" :
+        lib_dirs.append(os.path.join(MTI_HOME,"win32"))
+    else :
+        lib_dirs.append(os.path.join(MTI_HOME,"linux"))
+    libs.append("mtipli")
+elif VCS_HOME :
+    pass
+else :
+    print "Can't find variables : MTI_HOME, VCS_HOME."
+    quit(0)
+
+setup(name = '',
+      version = '1.0',
+      description = '',
+      author = 'Gene Kong',
+      author_email = 'genekong.sw@gmail.com',
+      url = 'https://github.com/gyx2545965/pyvpi',
+      py_modules = ['pyvpi_cons'],
+      ext_modules = [Extension('pyvpi',
+                               c_src,
+                               include_dirs = inc_dirs,
+                               library_dirs = lib_dirs,
+                               libraries = libs
+                               )]
+      )
